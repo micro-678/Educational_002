78b0717c3855ffafa917d7c1a732ca9c2027bfbc
micro-678 <dvimukthiwizard@gmail.com>
2025-04-20 08:51:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Education Assistant</h1>
+      <p>Upload your study material and get quizzes, flashcards, and revision help.</p>
+    </div>
+  );
+}
+
+export default App;
